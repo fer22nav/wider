7418094dd5efe75ba796dddb21697479570f59a5
fer22nav <fer22navarrete@gmail.com>
2024-09-27 16:24:08 -0300

    added privacy and statement translation

--- src/pages/AccessibilityStatement.js
+++ src/pages/AccessibilityStatement.js
@@ -1,115 +1,92 @@
-import React from 'react'
+import React from 'react';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useTranslation } from 'react-i18next';
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['accessibilityStatement', 'footer'])),
+    },
+  };
+}
 
 export default function AccessibilityStatement() {
+  const { t } = useTranslation('accessibilityStatement');
+
   return (
     <div>
-      <h1 className="titulo-xxl color-white pt-180 mb-50">Declaración de <br /> accesibilidad</h1>
-      <p className='h2-privacy-policy mb-50'>  <span className="color-lima">Wider,</span> entidad privada pone a disposición de sus usuarios el sitio<br />  web wider  (http://wider.global) con el que pretende ofrecer toda<br />
-        la comunicación de interés a los usuarios de la manera más<br />
-        accesible, al mismo tiempo que intenta suprimir las posibles <br />  barreras que impiden el adecuado acceso. </p>
+      <h1 className="titulo-xxl color-white pt-180 mb-50" dangerouslySetInnerHTML={{ __html: t('title') }}></h1>
 
-
+      <p className="h2-privacy-policy mb-50">
+        <span className="color-lima">{t('wider')}</span> {t('entity_message')}
+      </p>
+      {/* 
       <p className="p-nav color-white mb-50">
-        Para lograr estos fines, el sitio web se ha desarrollado para cumplir con el Real Decreto 1112/2018, de 7 de septiembre, sobre accesibilidad de los sitios web y aplicaciones para dispositivos móviles del sector público. El enlace se abre en una nueva ventana
-      </p>
-      <p className="p-nav color-white mb-50">
-        Para lograr estos fines, el sitio web se ha desarrollado para cumplir con el Real Decreto 1112/2018, de 7 de septiembre, sobre accesibilidad de los sitios web y aplicaciones para dispositivos móviles del sector público. El enlace se abre en una nueva ventana
-        Al mismo tiempo, el portal siempre ha tenido como objetivo adaptarse a los estándares y normativas vigentes en relación a la accesibilidad digital, cumpliendo con las Pautas de Accesibilidad al Contenido en la Web (WCAG 2.1) nivel de conformidad AA pertenecientes al W3C (World Wide Web Consortium).
-        La presente declaración de accesibilidad se aplica al sitio web http://www.wider.global
-      </p>
-      <p className="p-nav color-white mb-50">
-        La presente declaración de accesibilidad se aplica al sitio web http://www.wider.global
-      </p>
-      <h2 className='h2-privacy-policy'>Situación de cumplimiento</h2>
-      <p className="p-nav color-white">
-        Este sitio web es parcialmente conforme con el RD 1112/2018. La falta de conformidad de los aspectos que se indican a continuación:
+        {t('regulation_info')}
       </p>
 
-      <h2 className="h2-privacy-policy">Contenido no accesible</h2>
+      <p className="p-nav color-white mb-50" dangerouslySetInnerHTML={{ __html: t('compliance') }}></p> */}
 
-      <p className="p-nav color-white">
-        Aunque el portal se encuentra en revisión constante para solucionar todos los defectos encontrados, se tiene constancia de que los siguientes puntos tienen falta de conformidad con el RD 1112/2018 y no son accesibles:
-      </p>
+      <h2 className="h2-privacy-policy">{t('compliance_status.title')}</h2>
+      <p className="p-nav color-white">{t('compliance_status.content')}</p>
+
+      <h2 className="h2-privacy-policy">{t('non_accessible_content.title')}</h2>
+      <p className="p-nav color-white">{t('non_accessible_content.content')}</p>
 
       <ul className="p-nav color-white mb-50">
         <li>
-          a. Falta de conformidad con el RD 1112/2018
+          {t('non_accessible_content.non_compliance')}
           <ol>
-            <li>Cierta información, estructura o relaciones comunicadas a través de la presentación no pueden ser determinadas por software o no están disponibles como texto [requisitos números 9.4.1.3 Mensajes de estado y 9.1.3.1 Información y relaciones, de UNE-EN 301549:2022].</li>
-            <li>Podrían existir fallos puntuales de edición en alguna página web, tanto en contenidos HTML como en documentos finales, publicados en fecha posterior al 20 de septiembre de 2018 (fecha de entrada en vigor del Real Decreto 1112/2018, de 7 de septiembre).</li>
+            <li>{t('non_accessible_content.point1')}</li>
+            <li>{t('non_accessible_content.point2')}</li>
           </ol>
         </li>
-        <li>
-          b. Carga desproporcionada
-          <p className="p-nav color-white">No aplica.</p>
+        <li dangerouslySetInnerHTML={{ __html: t('non_accessible_content.burden') }}>
         </li>
-        <li>
-          c. El contenido no entra dentro del ámbito de la legislación aplicable
-          <p className="p-nav color-white">
-            Pueden existir archivos ofimáticos en PDF u otros formatos publicados antes del 20 de septiembre de 2018 que no cumplan en su totalidad todos los requisitos de accesibilidad. ENAIRE se compromete a proporcionar sus documentos PDF en formato accesible a quien lo solicite a través del correo <a href="mailto:accesibilidad@wider.global" className="color-white">accesibilidad@wider.global</a>.
-          </p>
+        <li dangerouslySetInnerHTML={{ __html: t('non_accessible_content.legislation') }}>
         </li>
       </ul>
 
-      <h2 className="h2-privacy-policy">Datos de contacto</h2>
-      <p className="p-nav color-white">Datos de contacto para temas de accesibilidad</p>
+      <h2 className="h2-privacy-policy">{t('statement_title')}</h2>
+      <p className="p-nav color-white mb-50" dangerouslySetInnerHTML={{ __html: t('statement_p') }}></p>
+      <p className="p-nav color-white mb-50">{t('statement_review')}</p>
 
-      <p className="p-nav color-white">
-        Puede realizar comunicaciones sobre requisitos de accesibilidad (artículo 10.2.a) del RD 1112/2018), como por ejemplo:
-      </p>
+      <h2 className="h2-privacy-policy">{t('contact_observations.title')}</h2>
+      <p className="p-nav color-white">{t('contact_observations.description')}</p>
 
       <ul className="p-nav color-white">
-        <li>Informar sobre cualquier posible incumplimiento por parte de este sitio web.</li>
-        <li>Transmitir otras dificultades de acceso al contenido.</li>
-        <li>Formular cualquier otra consulta o sugerencia de mejora relativa a la accesibilidad del sitio web.</li>
+        <li>{t('contact_observations.point1')}</li>
+        <li>{t('contact_observations.point2')}</li>
+        <li>{t('contact_observations.point3')}</li>
       </ul>
 
       <p className="p-nav color-white mb-50">
-        Para ello, puede escribirnos un correo a <a href="mailto:accesibilidad@wider.global" className="color-white">accesibilidad@wider.global</a>.
+        {t('contact_observations.email_prefix')}
+        <a href="mailto:accesibilidad@wider.global" className="color-white"> accesibilidad@wider.global</a>.
       </p>
 
-      <h2 className="h2-privacy-policy">Quejas y reclamaciones</h2>
-      <p className="p-nav color-white">Puede presentar:</p>
+      <h2 className="h2-privacy-policy">{t('enforcement_procedure.title')}</h2>
+      <p className="p-nav color-white">{t('enforcement_procedure.description')}</p>
 
       <ul className="p-nav color-white">
-        <li>Una queja relativa al cumplimiento de los requisitos del RD 1112/2018.</li>
-        <li>
-          Una solicitud de información accesible relativa a:
-          <ul>
-            <li>Contenidos que están excluidos del ámbito de aplicación del RD 1112/2018 según lo establecido por el artículo 3, apartado 4.</li>
-            <li>Contenidos que están exentos del cumplimiento de los requisitos de accesibilidad por imponer una carga desproporcionada.</li>
-          </ul>
-        </li>
+        <li>{t('enforcement_procedure.point1')}</li>
+        <li>{t('enforcement_procedure.point2')}</li>
       </ul>
 
-      <p className="p-nav color-white">
-        En la solicitud de información accesible, se debe concretar, con toda claridad, los hechos, razones y petición que permitan constatar que se trata de una solicitud razonable y legítima.
-      </p>
+      <p className="p-nav color-white">{t('enforcement_procedure.extra_info')}</p>
 
-      <p className="p-nav color-white">
-        La solicitud puede ser presentada a través del procedimiento correspondiente de la sede del propio organismo, o a través del procedimiento correspondiente de la sede del Ministerio (si el organismo tiene sus procedimientos en el Ministerio), así como en el resto de opciones recogidas en la Ley 39/2015, de 1 de octubre, del Procedimiento Administrativo Común de las Administraciones Públicas.
-      </p>
+      <ul className="p-nav color-white">
+        <li>
+          {t('enforcement_procedure.defensor_title')}:
+          <a href="http://www.defensordelpueblo.es" className="color-white"> www.defensordelpueblo.es</a>
+        </li>
+        <li>{t('enforcement_procedure.contact')}</li>
+      </ul>
 
-      <p className="p-nav color-white mb-50">
-        Las comunicaciones, quejas y solicitudes de información accesible serán recibidas y tratadas por Wider.
-      </p>
-
-      <h2 className="h2-privacy-policy">Preparación de la presente declaración de accesibilidad</h2>
-      <p className="p-nav color-white">La presente declaración de accesibilidad fue preparada el 30 de septiembre de 2024.
-        El método empleado para la presente declaración ha sido una autoevaluación realizada por Wider. En la autoevaluación se han realizado revisiones manuales y automáticas
-      </p>
-      <p className="p-nav color-white mb-50">Última revisión de la declaración: 30 de septiembre de 2024.</p>
-
-      <h2 className="h2-privacy-policy">Procedimiento de aplicación</h2>
-      <p className="p-nav color-white mb-50">
-        Si una vez realizada una solicitud de información accesible o queja, ésta hubiera sido desestimada, no se estuviera de acuerdo con la decisión adoptada, o la respuesta no cumpliera los requisitos contemplados en el artículo 12.5, la persona interesada podrá iniciar una reclamación. Igualmente se podrá iniciar una reclamación en el caso de que haya trascurrido el plazo de veinte días hábiles sin haber obtenido respuesta.
-        La reclamación puede ser presentada a través del procedimiento correspondiente de la sede del propio organismo, o a través del procedimiento correspondiente de la sede del Ministerio (si el organismo tiene sus procedimientos en el Ministerio), así como en el resto de opciones recogidas en la Ley 39/ 2015, de 1 de octubre, del Procedimiento Administrativo Común de las Administraciones Públicas.
-        Las reclamaciones serán recibidas y tratadas por wider.
-      </p>
-
-      <h2 className="p-xl color-white mb-0  mb-50">Si querés saber más, solicita una cita con<br /> nuestros profesionales en
-
-        <span className="color-lima"> info@wider.global</span></h2>
+      <h2
+        className="p-xl color-white mb-0 mb-50"
+        dangerouslySetInnerHTML={{ __html: t('learn_more_with_email') }}
+      ></h2>
     </div>
-  )
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 export async function getStaticProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common', 'footer'])),
+      ...(await serverSideTranslations(locale, ['common', 'footer', 'accessibilityStatement', 'privacyPolicy'])),
     },
   };
 }
